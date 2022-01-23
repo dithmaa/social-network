af78cca4e08c8f4fd952c6f55ae904500f82fa5c
Baltukov Nomto <nomto_04@mail.ru>
2022-01-23 13:31:04 +0800

    Updates

---
+++ precache-manifest.406342948b9dd7bc98bf66ad34bd5ee0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f52171d8ef736b688eef",
+    "url": "/social-network/static/css/main.626bbe42.chunk.css"
+  },
+  {
+    "revision": "f52171d8ef736b688eef",
+    "url": "/social-network/static/js/main.f52171d8.chunk.js"
+  },
+  {
+    "revision": "a3f06aa5ed20eddffa29",
+    "url": "/social-network/static/js/1.a3f06aa5.chunk.js"
+  },
+  {
+    "revision": "1faa7afe6abd52a90e16",
+    "url": "/social-network/static/js/2.1faa7afe.chunk.js"
+  },
+  {
+    "revision": "3311e592bc867089450c",
+    "url": "/social-network/static/js/runtime~main.3311e592.js"
+  },
+  {
+    "revision": "e3f1b2d3b7f74ccb9a724e486c4ba72b",
+    "url": "/social-network/static/media/avatar.e3f1b2d3.png"
+  },
+  {
+    "revision": "f891bc99b8647d57f2e6acc5e7eb8fb8",
+    "url": "/social-network/index.html"
+  }
+];
